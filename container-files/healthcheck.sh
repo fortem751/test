60767236bbad9f13e906fbe5ed2aa6b411dbc3d9
fortem751 <fortem751@gmail.com>
2016-12-05 15:48:48 +0100

    Create healthcheck.sh

---
+++ container-files/healthcheck.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+httptest=$(curl -w %{http_code} -s --output /dev/null localhost:7474)
+if [ "netstat -plunt | grep 5001" ]; then
+  echo "successfully connected to port 5001"
+  if [ "netstat -plunt | grep 6001" ]; then  
+    echo "successfully connected to port 6001"
+    if [[ $httptest -ne 200 ]]; then
+     if [ -f /data/first_run ]; then 
+       echo "lost connection to port 7474"
+       exit 1 
+     else
+       echo "still creating a cluster exiting successfully"
+       
+       exit 0
+     fi
+    else
+     if [ -f /data/first_run ]; then
+      echo "connected to port 7474 successfully"
+      exit 0 
+     else 
+      touch /data/first_run
+      echo "connected to port 7474 for the first time"
+      exit 0
+     fi
+    fi
+  else 
+    echo "port 6001 not available exiting"   
+    exit 1
+  fi
+else 
+  echo "port 5001 not available exiting"
+  exit 1
+fi
